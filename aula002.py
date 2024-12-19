c19ddaa243a9a200f59de77be84accbb9b892359
guedes1818 <daviguedescontato17@gmail.com>
2024-12-19 11:21:25 -0300

    Add files via upload

---
+++ aula002.py
@@ -0,0 +1,8 @@
+'''
+Aulas sobre váriaveis 
+19/02/24
+Davi Guedes
+'''
+x=2
+print(x)
+print(x+x+x)
---
+++ aula003.py
@@ -0,0 +1,14 @@
+'''
+Modificação de variáveis;
+Davi Guedes
+19/12/2024
+
+'''
+
+x=str(3)
+y=int(4)
+z=float(5)
+
+print(x+x)
+print(y+y)
+print(z+z)
