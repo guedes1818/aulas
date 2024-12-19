8852fb9edfbacd6d0235903255213644a293b277
guedes1818 <daviguedescontato17@gmail.com>
2024-12-19 11:41:12 -0300

    Add files via upload

---
+++ aula004.py
@@ -0,0 +1,12 @@
+'''
+Modificação de dados em strings!
+19/02
+Davi Guedes
+'''
+
+#O Davi tem 18 anos de idade e mora na cidade de ceilandia df.
+nome='Davi'
+idade= str(18)
+cidade='Ceilandia'
+
+print('O ' + nome + ' tem a idade de  ' + idade + ' E mora em  ' + cidade)
