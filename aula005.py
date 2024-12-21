f1b9b59495ed39bca51f582b2ebf8deb00a24031
guedes1818 <daviguedescontato17@gmail.com>
2024-12-21 18:04:09 -0300

    Add files via upload

---
+++ aula005.py
@@ -0,0 +1,12 @@
+'''
+21/12/24
+Davi Guedes 
+Escrevendo um programa para calcular a idade da pessoa!
+'''
+ano_nascimento=input('Qual o ano do seu nascimento?')
+idade=2024- int(ano_nascimento)
+print(idade)
+
+
+
+
